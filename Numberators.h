ac62b713005cb7ab8b085202c68c1967947558f9
Dimezske <dimezske@gmail.com>
2016-10-13 11:45:50 +1100

    The Numberators Header File
    This Header File Contains The three Function Prototypes from The Numberators.cpp source File

---
+++ Numberators.h
@@ -0,0 +1,9 @@
+#ifndef NUMBERATORS_H_INCLUDED
+#define NUMBERATORS_H_INCLUDED
+
+
+int numberRator(int param1, int *a);
+void numberRator2(int param2,int *b);
+void abbacuS(int *green, int *blue, int *red,int *orange,int *purple);
+
+#endif //NUMBERATORS_H_INCLUDED
